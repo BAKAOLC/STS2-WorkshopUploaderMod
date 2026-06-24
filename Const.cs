0b0e7ae8d3678523154fc53956301e6eadacae5c
OLC <utsumabo@ritsukage.com>
2026-06-24 17:56:52 +0800

    Update version to 0.1.4 in Const.cs and mod_manifest.json

--- Const.cs
+++ Const.cs
@@ -3,6 +3,6 @@ namespace STS2WorkshopUploader;
 internal static class Const
 {
     public const string ModId = "STS2-WorkshopUploader";
-    public const string Version = "0.1.3";
+    public const string Version = "0.1.4";
     public const uint Sts2SteamAppId = 2868840;
 }
